5a0033a294d19e85ccbb75daaf5c837c50a814d4
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2014-08-05 23:45:10 +0400

    Доработано решение 3.6.4

--- src/com/javarush/test/level03/lesson06/task04/Solution.java
+++ src/com/javarush/test/level03/lesson06/task04/Solution.java
@@ -4,14 +4,12 @@ package com.javarush.test.level03.lesson06.task04;
 Выведи на экран текст: полное имя файла java.exe из установленного пакета java
 */
 
-import java.util.Arrays;
-
 public class Solution
 {
     public static void main(String[] args)
     {
         //Напишите тут ваш код
-        String path = String.formast("%s\\bin\\java.exe", System.getProperty("java.home")) ;
+        String path = String.format("%s\\bin\\java.exe", System.getProperty("java.home")) ;
         System.out.print(path);
     }
 }
