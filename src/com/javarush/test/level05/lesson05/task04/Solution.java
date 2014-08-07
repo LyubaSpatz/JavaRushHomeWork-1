ade3896e816f6a7ac3b705e66e746088d8b0d7ea
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2014-08-07 19:25:32 +0400

    Добавлено решение 5.5.4

---
+++ src/com/javarush/test/level05/lesson05/task04/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.test.level05.lesson05.task04;
+
+/* Создать три объекта типа Cat
+В методе main создать три объекта типа Cat и заполнить их данными.
+Использовать класс Cat из первой задачи. Класс Cat создавать не надо.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //add your code here
+        for (int i = 0; i < 3; i++) {
+            new Cat(String.format("Name %d",i),i,i,i );
+        }
+    }
+
+    public static class Cat {
+
+        public static int count = 0;
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            count++;
+
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
